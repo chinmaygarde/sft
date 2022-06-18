68c3c01a420f62c637b3f18587f9738bdfa2d749
Chinmay Garde <chinmaygarde@gmail.com>
2022-06-17 19:25:52 -0700

    Bug

--- src/rasterizer.cc
+++ src/rasterizer.cc
@@ -35,7 +35,7 @@ size_t Rasterizer::GetDepthBytesPerPixel() const {
 }
 
 constexpr bool IsOOB(glm::ivec2 pos, glm::ivec2 size) {
-  return pos.x < 0 || pos.y < 0 || -pos.x > size.x || pos.y > size.y;
+  return pos.x < 0 || pos.y < 0 || pos.x > size.x || pos.y > size.y;
 }
 
 void Rasterizer::UpdateTexel(Texel texel) {
