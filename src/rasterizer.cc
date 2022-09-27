52fe68f545d26bb47a7a70d4b98ede8528fa2632
Chinmay Garde <chinmaygarde@gmail.com>
2022-09-27 00:02:51 -0700

    Fix OOB check.
    Change-Id: If94a913598e1580a98526b750f8babbbff11a236

--- src/rasterizer.cc
+++ src/rasterizer.cc
@@ -28,7 +28,7 @@ size_t Rasterizer::GetBytesPerPixel() const {
 }
 
 constexpr bool IsOOB(glm::ivec2 pos, glm::ivec2 size) {
-  return pos.x < 0 || pos.y < 0 || pos.x > size.x || pos.y > size.y;
+  return pos.x < 0 || pos.y < 0 || pos.x >= size.x || pos.y >= size.y;
 }
 
 bool Rasterizer::FragmentPassesDepthTest(const Pipeline& pipeline,
