2136466307ca9ffa7e79920b6b6ddedc32c4b33a
Chinmay Garde <chinmaygarde@google.com>
2022-09-30 16:05:58 -0700

    Fix issues accounting for strides.
    Change-Id: I155a0260abf12f71456e0e6ec354e6151d2ebb6a

--- src/rasterizer.cc
+++ src/rasterizer.cc
@@ -236,8 +236,7 @@ void Rasterizer::Draw(const Pipeline& pipeline,
                       const Buffer& uniform_buffer,
                       size_t count) {
   const auto& vtx_desc = pipeline.vertex_descriptor;
-  const auto* vtx_ptr = reinterpret_cast<const glm::vec3*>(
-      vertex_buffer.GetData() + vtx_desc.offset);
+  const auto* vtx_ptr = vertex_buffer.GetData() + vtx_desc.offset;
   const auto varyings_size = pipeline.shader->GetVaryingsSize();
   auto* varyings = reinterpret_cast<uint8_t*>(::alloca(varyings_size * 3u));
   TriangleData data(pipeline,        //
@@ -248,9 +247,12 @@ void Rasterizer::Draw(const Pipeline& pipeline,
   );
   for (size_t i = 0; i < count; i += 3) {
     data.base_vertex_id = i;
-    memcpy(&data.p1, &vtx_ptr[i + 0], sizeof(glm::vec3));
-    memcpy(&data.p2, &vtx_ptr[i + 1], sizeof(glm::vec3));
-    memcpy(&data.p3, &vtx_ptr[i + 2], sizeof(glm::vec3));
+    memcpy(&data.p1, vtx_ptr, sizeof(glm::vec3));
+    vtx_ptr += vtx_desc.stride;
+    memcpy(&data.p2, vtx_ptr, sizeof(glm::vec3));
+    vtx_ptr += vtx_desc.stride;
+    memcpy(&data.p3, vtx_ptr, sizeof(glm::vec3));
+    vtx_ptr += vtx_desc.stride;
     DrawTriangle(data);
   }
 }
