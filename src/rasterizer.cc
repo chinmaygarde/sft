e51f2011d658fd4d05bd2a99adf8fdbbcbafa9d0
Chinmay Garde <chinmaygarde@google.com>
2022-10-01 01:24:49 -0700

    Remove unused comments.
    Change-Id: I70afd6ffc99a79f433567e2c9287f2a4b68bda65

--- src/rasterizer.cc
+++ src/rasterizer.cc
@@ -61,8 +61,6 @@ void Rasterizer::UpdateTexel(const Pipeline& pipeline, Texel texel) {
   //----------------------------------------------------------------------------
   // Write to the color attachment.
   //----------------------------------------------------------------------------
-  // std::cout << "Updating texel: " << texel.pos.x << "," << texel.pos.y
-  //           << std::endl;
   auto color_ptr = reinterpret_cast<uint32_t*>(color_buffer_) + offset;
   *color_ptr = pipeline.Blend(texel.color, *color_ptr);
 
