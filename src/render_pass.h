f97b06fdb0d42dbb1e6dcb49d5eed1ee7d9438c3
Chinmay Garde <chinmaygarde@google.com>
2022-10-04 21:40:54 -0700

    Make progress on render passes.
    Change-Id: Ia5e963a9b7337925c9a56388dc31fe0c84fc97bd

--- src/render_pass.h
+++ src/render_pass.h
@@ -21,9 +21,10 @@ enum class StoreAction {
 struct PassAttachment {
   LoadAction load_action = LoadAction::kDontCare;
   StoreAction store_action = StoreAction::kDontCare;
-  std::shared_ptr<FramebufferBase> texture;
 
-  bool IsValid() const { return !!texture; }
+  virtual glm::ivec2 GetSize() const = 0;
+
+  virtual bool IsValid() const = 0;
 
   virtual void Load() = 0;
 
@@ -32,24 +33,66 @@ struct PassAttachment {
 
 struct ColorPassAttachment final : public PassAttachment {
   glm::vec4 clear_color = {0.0, 0.0, 0.0, 1.0};
+  std::shared_ptr<Framebuffer<Color>> texture;
 
-  void Load() override {}
+  glm::ivec2 GetSize() const override { return texture->GetSize(); }
+
+  bool IsValid() const override { return !!texture; }
+
+  void Load() override {
+    switch (load_action) {
+      case LoadAction::kDontCare:
+      case LoadAction::kLoad:
+        break;
+      case LoadAction::kClear:
+        texture->Clear(clear_color);
+        break;
+    }
+  }
 
   void Store() override {}
 };
 
 struct DepthPassAttachment : public PassAttachment {
   ScalarF clear_depth = 1.0;
+  std::shared_ptr<Framebuffer<ScalarF>> texture;
 
-  void Load() override {}
+  glm::ivec2 GetSize() const override { return texture->GetSize(); }
+
+  bool IsValid() const override { return !!texture; }
+
+  void Load() override {
+    switch (load_action) {
+      case LoadAction::kDontCare:
+      case LoadAction::kLoad:
+        break;
+      case LoadAction::kClear:
+        texture->Clear(clear_depth);
+        break;
+    }
+  }
 
   void Store() override {}
 };
 
 struct StencilPassAttachment : public PassAttachment {
   uint32_t clear_stencil = 0;
+  std::shared_ptr<Framebuffer<uint32_t>> texture;
 
-  void Load() override {}
+  glm::ivec2 GetSize() const override { return texture->GetSize(); }
+
+  bool IsValid() const override { return !!texture; }
+
+  void Load() override {
+    switch (load_action) {
+      case LoadAction::kDontCare:
+      case LoadAction::kLoad:
+        break;
+      case LoadAction::kClear:
+        texture->Clear(clear_stencil);
+        break;
+    }
+  }
 
   void Store() override {}
 };
@@ -59,23 +102,35 @@ struct RenderPass {
   DepthPassAttachment depth;
   StencilPassAttachment stencil;
 
-  // bool IsValid() const {
-  //   if (!color.IsValid() || !depth.IsValid() || !stencil.IsValid()) {
-  //     return false;
-  //   }
-  //   auto size = color.texture->GetSize();
-  //   if (!glm::equal(size, depth.texture->GetSize())) {
-  //     return false;
-  //   }
+  bool IsValid() const {
+    if (!color.IsValid() || !depth.IsValid() || !stencil.IsValid()) {
+      return false;
+    }
+    const auto texture_size = color.texture->GetSize();
+    const auto depth_size = depth.texture->GetSize();
+    const auto stencil_size = stencil.texture->GetSize();
+    return texture_size == depth_size && texture_size == stencil_size;
+  }
 
-  //   if (!glm::equal(size, stencil.texture->GetSize())) {
-  //     return false;
-  //   }
-  // }
+  [[nodiscard]] bool Begin() {
+    if (!IsValid()) {
+      return false;
+    }
+    color.Load();
+    depth.Load();
+    stencil.Load();
+    return true;
+  }
 
-  void Begin() {}
-
-  void End() {}
+  [[nodiscard]] bool End() {
+    if (!IsValid()) {
+      return false;
+    }
+    color.Store();
+    depth.Store();
+    stencil.Store();
+    return true;
+  }
 };
 
 }  // namespace sft
