097d9845626797c04a334cc1afd8dca36249c4df
Chinmay Garde <chinmaygarde@gmail.com>
2023-06-11 10:44:20 -0700

    Move geometry stuff into its own TUs.

---
+++ geometry/types.h
@@ -0,0 +1,25 @@
+/*
+ *  This source file is part of the SFT project.
+ *  Licensed under the MIT License. See LICENSE file for details.
+ */
+
+#pragma once
+
+#include <cstdint>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/compatibility.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+namespace sft {
+
+using Scalar = int32_t;
+using ScalarF = float;
+
+constexpr ScalarF kEpsilon = 1e-5;
+
+constexpr glm::vec2 kSampleMidpoint = {0.5f, 0.5f};
+
+}  // namespace sft
