90a7d29637ce712e430e7a75e4e5290636e6461d
Chinmay Garde <chinmaygarde@google.com>
2023-09-26 16:07:26 -0700

    Start wiring up benchmarks.
    Change-Id: Ic9d586b29c282425080aa09d3b1699bf8c31ee59

--- src/playground/benchmarks.cc
+++ src/playground/benchmarks.cc
@@ -1,14 +1,42 @@
 #include <benchmark/benchmark.h>
+#include "rasterizer.h"
 
 namespace sft {
 
-#define DEF_BENCH(name)                           \
-  static void BM_##name(benchmark::State& state); \
-  BENCHMARK(BM_##name);                           \
-  static void BM_##name(benchmark::State& state)
+#define ADD_BENCHMARK(name) \
+  BENCHMARK_F(Benchmark, name)(benchmark::State & state)
 
-DEF_BENCH(SomeFunction) {
+class BenchmarkFixture : public ::benchmark::Fixture {
+ public:
+  BenchmarkFixture() : scheduler_(marl::Scheduler::Config::allCores()) {}
+
+  void SetUp(const ::benchmark::State& state) override { scheduler_.bind(); }
+
+  void TearDown(const ::benchmark::State& state) override {
+    scheduler_.unbind();
+  }
+
+ private:
+  marl::Scheduler scheduler_;
+
+  SFT_DISALLOW_COPY_AND_ASSIGN(BenchmarkFixture);
+};
+
+using Benchmark = BenchmarkFixture;
+
+constexpr glm::ivec2 kDefaultRasterizerSize = {1 << 14, 1 << 14};
+
+ADD_BENCHMARK(DoNothing) {
   for (auto _ : state) {
+  }
+}
+
+ADD_BENCHMARK(Clear) {
+  Rasterizer rasterizer(kDefaultRasterizerSize, SampleCount::kFour);
+  for (auto _ : state) {
+    rasterizer.ResetMetrics();
+    rasterizer.Clear(kColorFirebrick);
+    rasterizer.Finish();
   }
 }
 
